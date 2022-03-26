fb8390cef77c5ed7759799e442cced5585fa774a
Peter Ferri <pjferri10@gmail.com>
2022-03-25 21:00:57 -0400

    Cards now create the main table layout on play

--- Assets/Prospector/__Scripts/Card.cs
+++ Assets/Prospector/__Scripts/Card.cs
@@ -2,54 +2,107 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class Card : MonoBehaviour {
+public class Card : MonoBehaviour
+{
+	public string suit;
+	public int rank;
+	public Color color = Color.black;
+	public string colS = "Black";
 
-	public string    suit;
-	public int       rank;
-	public Color     color = Color.black;
-	public string    colS = "Black";  // or "Red"
-	
 	public List<GameObject> decoGOs = new List<GameObject>();
+
 	public List<GameObject> pipGOs = new List<GameObject>();
-	
-	public GameObject back;  // back of card;
-	public CardDefinition def;  // from DeckXML.xml		
 
+	public GameObject back; 
+	public CardDefinition def; 
 
-	public bool faceUp {
-		get {
+	public SpriteRenderer[] spriteRenderers;
+
+	void Start()
+	{
+		SetSortOrder(0);
+	}
+
+	public bool faceUp
+	{
+		get
+		{
 			return (!back.activeSelf);
 		}
-
-		set {
+		set
+		{
 			back.SetActive(!value);
 		}
 	}
 
 
-	// Use this for initialization
-	void Start () {
-	
+	public void PopulateSpriteRenderers()
+	{
+		if (spriteRenderers == null || spriteRenderers.Length == 0)
+		{
+			spriteRenderers = GetComponentsInChildren<SpriteRenderer>();
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-} // class Card
 
-[System.Serializable]
-public class Decorator{
-	public string	type;			// For card pips, tyhpe = "pip"
-	public Vector3	loc;			// location of sprite on the card
-	public bool		flip = false;	//whether to flip vertically
-	public float 	scale = 1.0f;
+	public void SetSortingLayerName(string tSLN)
+	{
+		PopulateSpriteRenderers();
+
+		foreach (SpriteRenderer tSR in spriteRenderers)
+		{
+			tSR.sortingLayerName = tSLN;
+		}
+	}
+
+	public void SetSortOrder(int sOrd)
+	{
+		PopulateSpriteRenderers();
+
+		foreach (SpriteRenderer tSR in spriteRenderers)
+		{
+			if (tSR.gameObject == this.gameObject)
+			{
+				tSR.sortingOrder = sOrd;
+				continue;
+			}
+			switch (tSR.gameObject.name)
+			{
+				case "back":
+					tSR.sortingOrder = sOrd + 2;
+					break;
+				case "face":
+				default:
+					tSR.sortingOrder = sOrd + 1;
+					break;
+			}
+		}
+	}
+
+	//Virtual methods can be overriden by subclass methods with the same name
+	virtual public void OnMouseUpAsButton()
+	{
+		//print(name);
+	}
 }
 
 [System.Serializable]
-public class CardDefinition{
-	public string	face;	//sprite to use for face cart
-	public int		rank;	// value from 1-13 (Ace-King)
-	public List<Decorator>	
-					pips = new List<Decorator>();  // Pips Used
+public class Decorator
+{
+	//This class stores information about each decorator or pop from DeckXML
+	public string type; //For card pips, type = "pip"
+	public Vector3 loc; //The location of the Sprite on the card
+	public bool flip = false; //Whether to flip the Sprite vertically
+	public float scale = 1f; //The scale of the sprite
+}
+
+[System.Serializable]
+public class CardDefinition
+{
+	//This class stores information for each rank of card.
+	public string face; //Sprite to use for each face card
+	public int rank; //The rank (1-13) of this card
+
+	//Pips used. Because decorators (from the XML) are used the same way on every card in the deck, 
+	//pips only stores information about the pips on numbered cards
+	public List<Decorator> pips = new List<Decorator>();
 }
--- Assets/Prospector/__Scripts/Prospector.cs
+++ Assets/Prospector/__Scripts/Prospector.cs
@@ -5,26 +5,36 @@ using System.Collections;
 using System.Collections.Generic;
 
 
-public class Prospector : MonoBehaviour {
+public class Prospector : MonoBehaviour
+{
 
-	static public Prospector 	S;
+	static public Prospector S;
 
 	[Header("Set in Inspector")]
-	public TextAsset			deckXML;
-	public TextAsset			layoutXML;
+	public TextAsset deckXML;
+	public TextAsset layoutXML;
+	public float xOffset = 3;
+	public float yOffset = -2.5f;
+	public Vector3 layoutCenter;
 
 
 	[Header("Set Dynamically")]
-	public Deck					deck;
-	public Layout				layout;
+	public Deck deck;
+	public Layout layout;
 	public List<CardProspector> drawPile;
+	public Transform layoutAnchor;
+	public CardProspector target;
+	public List<CardProspector> tableau;
+	public List<CardProspector> discardPile;
 
-	void Awake(){
+	void Awake()
+	{
 		S = this;
 	}
 
-	void Start() {
-		deck = GetComponent<Deck> ();
+	void Start()
+	{
+		deck = GetComponent<Deck>();
 		deck.InitDeck(deckXML.text);
 		Deck.Shuffle(ref deck.cards);
 
@@ -38,6 +48,7 @@ public class Prospector : MonoBehaviour {
 		layout = GetComponent<Layout>();
 		layout.ReadLayout(layoutXML.text);
 		drawPile = ConvertListCardsToListCardProspectors(deck.cards);
+		LayoutGame();
 	}
 
 	List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD)
@@ -50,5 +61,42 @@ public class Prospector : MonoBehaviour {
 			lCP.Add(tCP);
 		}
 		return (lCP);
+	}
+
+	CardProspector Draw()
+	{
+		CardProspector cd = drawPile[0];
+		drawPile.RemoveAt(0);
+		return (cd);
+	}
+
+	void LayoutGame()
+	{
+		if (layoutAnchor == null)
+		{
+			GameObject tGO = new GameObject("_LayoutAnchor"); 
+			layoutAnchor = tGO.transform; 
+			layoutAnchor.transform.position = layoutCenter; 
+		}
+
+		CardProspector cp;
+		foreach (SlotDef tSD in layout.slotDefs)
+		{
+			cp = Draw();
+			cp.faceUp = tSD.faceUp; 
+			cp.transform.parent = layoutAnchor;
+
+			cp.transform.localPosition = new Vector3(
+				layout.multiplier.x * tSD.x,
+				layout.multiplier.y * tSD.y,
+				-tSD.layerID);
+
+			cp.layoutID = tSD.id;
+			cp.slotDef = tSD;
+			cp.state = CardState.tableau;
+			cp.SetSortingLayerName(tSD.layerName);
+
+			tableau.Add(cp);
+		}
 	}
 }
